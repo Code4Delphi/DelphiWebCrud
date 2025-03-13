1d422b0d3d154d4fa0999903b6370ed7055ab33b
César Cardoso <contato@code4delphi.com.br>
2025-03-13 03:16:37 -0300

    bk/2025-03-13-0310

--- Client/Src/Main.View.pas
+++ Client/Src/Main.View.pas
@@ -10,7 +10,23 @@ uses
   WEBLib.Graphics,
   WEBLib.Controls,
   WEBLib.Forms,
-  WEBLib.Dialogs, Vcl.Controls, Vcl.StdCtrls, WEBLib.StdCtrls;
+  WEBLib.Dialogs,
+  Vcl.Controls,
+  Vcl.StdCtrls,
+  WEBLib.StdCtrls,
+  VCL.TMSFNCTypes,
+  VCL.TMSFNCUtils,
+  VCL.TMSFNCGraphics,
+  VCL.TMSFNCGraphicsTypes,
+  System.Rtti,
+  VCL.TMSFNCDataGridCell,
+  VCL.TMSFNCDataGridData,
+  VCL.TMSFNCDataGridBase,
+  VCL.TMSFNCDataGridCore,
+  VCL.TMSFNCDataGridRenderer,
+  VCL.TMSFNCCustomControl,
+  VCL.TMSFNCDataGrid, WEBLib.ExtCtrls, WEBLib.WebCtrls, VCL.TMSFNCCustomComponent, VCL.TMSFNCDataGridDatabaseAdapter,
+  Data.DB, WEBLib.DB, XData.Web.JsonDataset, XData.Web.Dataset, XData.Web.Client, XData.Web.Connection;
 
 type
   TMainView = class(TWebForm)
@@ -19,12 +35,28 @@ type
     aInformation: TWebLabel;
     aConfirmation: TWebLabel;
     aCustom: TWebLabel;
+    WebResponsiveGridPanel1: TWebResponsiveGridPanel;
+    TMSFNCDataGrid1: TTMSFNCDataGrid;
+    XDataWebConnection1: TXDataWebConnection;
+    XDataWebClient1: TXDataWebClient;
+    XDataWebDataSet1: TXDataWebDataSet;
+    WebDataSource1: TWebDataSource;
+    TMSFNCDataGridDatabaseAdapter1: TTMSFNCDataGridDatabaseAdapter;
+    pnBotoes: TWebPanel;
+    btnGetNome: TWebButton;
+    WebButton1: TWebButton;
     procedure aWarningClick(Sender: TObject);
     procedure aErrorClick(Sender: TObject);
     procedure aInformationClick(Sender: TObject);
     procedure aConfirmationClick(Sender: TObject);
     procedure aCustomClick(Sender: TObject);
+    procedure btnGetNomeClick(Sender: TObject);
+    procedure XDataWebClient1Load(Response: TXDataClientResponse);
+    procedure XDataWebClient1Error(Error: TXDataClientError);
+    procedure WebButton1Click(Sender: TObject);
   private
+    procedure SuccessProc(Response: TXDataClientResponse);
+
   public
 
   end;
@@ -59,6 +91,49 @@ end;
 procedure TMainView.aCustomClick(Sender: TObject);
 begin
   MessageDlg('Mensagem mtCustom', mtCustom, [mbAbort, mbRetry, mbClose]);
+end;
+
+procedure TMainView.btnGetNomeClick(Sender: TObject);
+begin
+  XDataWebClient1.RawInvoke('IClientesService.GetNome', [4], @SuccessProc);
+end;
+
+procedure TMainView.SuccessProc(Response: TXDataClientResponse);
+begin
+  btnBuscar.Caption := TJSJson.stringify(Response.Result);
+end;
+
+procedure TMainView.XDataWebClient1Load(Response: TXDataClientResponse);
+begin
+  btnBuscar.Caption := TJSJson.stringify(Response.Result);
+
+//  if Response.RequestId = 'get' then
+//    btnBuscar.Caption := TJSJson.stringify(Response.Result);
+//  else
+//  if Response.RequestId = 'list' then
+//    ListMemo.Lines.Text := TJSJson.stringify(Response.Result)
+//  else
+//  if Response.RequestId = 'post' then
+//    ShowInfo(Format('New entity created in %s collection.', [SelectedEntitySet]))
+//  else
+//  if Response.RequestId = 'put' then
+//    ShowInfo(Format('Entity updated in %s collection.', [SelectedEntitySet]))
+//  else
+//  if Response.RequestId = 'delete' then
+//    ShowInfo(Format('Entity removed from %s collection.', [SelectedEntitySet]));
+end;
+
+procedure TMainView.XDataWebClient1Error(Error: TXDataClientError);
+begin
+  MessageDlg(Format('%s. RequestId: %s. Code: %s. Request Url: %s',
+    [Error.ErrorMessage, Error.RequestId, Error.ErrorCode, Error.RequestUrl]),
+    mtError, []
+  );
+end;
+
+procedure TMainView.WebButton1Click(Sender: TObject);
+begin
+  //
 end;
 
 end.
