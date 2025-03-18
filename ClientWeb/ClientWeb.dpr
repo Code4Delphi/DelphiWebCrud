51d71b684b02f8ab41f884ef4ff04777f7f586f0
César Cardoso <contato@code4delphi.com.br>
2025-03-18 10:49:11 -0300

    featyure/aula07

--- ClientWeb/ClientWeb.dpr
+++ ClientWeb/ClientWeb.dpr
@@ -7,13 +7,14 @@ program ClientWeb;
 uses
   Vcl.Forms,
   WEBLib.Forms,
-  Main.View in 'Src\Main.View.pas' {MainView: TWebForm} {*.html};
+  Main.View in 'Src\Main.View.pas' {MainView: TWebForm} {*.html},
+  Login.View in 'Src\Login.View.pas' {LoginView: TWebForm} {*.html};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TMainView, MainView);
+  Application.CreateForm(TLoginView, LoginView);
   Application.Run;
 end.
